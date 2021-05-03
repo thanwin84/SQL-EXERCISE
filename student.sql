8d35ae859beb403a3ae7cedacc6a451109304da3
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-05-03 13:19:41 +0600

    Update student.sql

--- student.sql
+++ student.sql
@@ -12,4 +12,29 @@ CREATE TABLE student(
     major VARCHAR(20),
     PRIMARY KEY(student_id)
 );
-
+--we can see the table
+DESCRIBE student;
+--we can also delete table
+DROP TABLE student;
+--WE can add another column to this table
+ALTER TABLE student ADD gpa DECIMAL(5, 2);
+--inserting data into table
+INSERT INTO student VALUES(1, 'Jack', 'Biology');
+--another way of inserting data into table
+INSERT INTO student(student_id, name) VALUES(1, 'claire);
+ --creating table
+--name cannot be null
+CREATE TABLE student(
+    student_id INT,
+    name VARCHAR(20) NOT NULL,
+    major VARCHAR(20) UNIQUE,
+    PRIMARY KEY(student_id)
+);
+ -- we can also set default value in our table
+                                             
+    CREATE TABLE student(
+    student_id INT,
+    name VARCHAR(20) NOT NULL,
+    major VARCHAR(20) DEFAULT 'undecided',
+    PRIMARY KEY(student_id)
+);
