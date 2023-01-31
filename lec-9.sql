fd2797cbbdc874f44d1d2a45e75227ada6b51027
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-02-01 01:11:58 +0600

    Update lec-9.sql

--- lec-9.sql
+++ lec-9.sql
@@ -225,8 +225,8 @@ INSERT INTO CUSTOMER(ID, CNAME, ADDRESS, GENDER, CITY, PINCODE) VALUES
 (301, "Rohan Arora", "Ludhiana H.O", "M", "Ludhiana", 141001);
 
 INSERT INTO ORDER_DETAILS(ORDER_ID, DELIVERY_DATE, CUST_ID) VALUES
-(008, '2022-03-10', 301),
-(001, '2022-01-02', 1300),
+(008, '2022-03-10', 100),
+(001, '2022-01-02', 102),
 (002, '2022-01-02', 1300);
 
 SELECT * FROM CUSTOMER;
@@ -246,5 +246,183 @@ INSERT INTO ORDER_DETAILS(ORDER_ID, DELIVERY_DATE, CUST_ID) VALUES
 DELETE FROM CUSTOMER WHERE ID = 301;
 DELETE FROM ORDER_DETAILS WHERE ORDER_ID = 8;
 
-SELECT * FROM CUSTOMER;
+
 -- using replace
+SELECT * FROM CUSTOMER;
+REPLACE INTO CUSTOMER (ID, CITY) VALUES(246, 'Dhaka'); 
+REPLACE INTO CUSTOMER (ID, CNAME, PINCODE) VALUES (	100, 'Jeema Oo', 20201);
+REPLACE INTO CUSTOMER SET ID = 102, CNAME = 'Bob Marley';
+-- random exerceise 
+SELECT CNAME FROM CUSTOMER ORDER BY CNAME;
+
+select * from order_details;
+
+CREATE DATABASE COMPANY;
+
+USE COMPANY;
+
+CREATE TABLE EMPLOYEE( 
+	ID INT NOT NULL PRIMARY KEY,
+    F_NAME VARCHAR(20),
+    L_NAME VARCHAR(20),
+    AGE INT,
+    EMAIL_ID VARCHAR(320),
+    PHONE_NO char(5),
+    CITY VARCHAR(25)
+);
+select * from EMPLOYEE;
+desc employee;
+INSERT INTO EMPLOYEE(ID, F_NAME, L_NAME, AGE, EMAIL_ID, PHONE_NO, CITY) VALUES 
+(10, 'Aman', 'Proto', 32, 'aman@gmail.com', '12345', 'Delhi'),
+(2, 'Yagya', 'Narayan', 44, 'yagya@gmail.com', '55231', 'Palam'),
+(3, 'Rahul', 'Bd', 22, 'rahul@gmail.com', '55243', 'Kolkata'),
+(4, 'Jatin', 'Helmet', 31, 'jatin@gmail.com', '65124', 'Rajpur'),
+(5, 'Pk', 'Pandey', 21, 'pk@gmail.com', '76121', 'Jaipur' );
+
+SELECT * FROM CLIENT;
+CREATE TABLE CLIENT( 
+	ID INT NOT NULL PRIMARY KEY,
+    F_NAME VARCHAR(25),
+    L_NAME VARCHAR(25),
+    AGE int, 
+    EMAIL_ID varchar(320),
+	PHONE_NO varchar(10),
+	CITY varchar(35),
+    EMP_REF_ID int,
+    FOREIGN KEY(EMP_REF_ID)
+    references EMPLOYEE(Id)
+);
+DESC CLIENT;
+INSERT INTO CLIENT(ID, F_NAME, L_NAME, AGE, EMAIL_ID, PHONE_NO, CITY, EMP_REF_ID) VALUES
+(1, 'Mac', 'Rogers', 47, 'mac@gmail.com', '33121', 'kolkata', 3),
+(2, 'Max', 'Porier', 27, 'peter@gmail.com', '12761', 'kolkata', 3),
+(3, 'Peter','jain', 24, 'peter@gmail.com', '12812', 'dehli', 1),
+(4, 'Sushant', 'Aggarwal', 23, 'sushant@yahoo.com', '62621', 'Hydrabad', 5),
+(5, 'Pratap', 'singh', 36, 'p@gmail.com', '77767', 'Mumbai', 2);
+
+select * from client;
+CREATE TABLE PROJECT (
+	ID INT NOT NULL PRIMARY KEY,
+    EMP_REF_ID INT, 
+	NAME varchar(25),
+    START_DATE DATETIME,
+    CLIENT_REF_ID INT,
+    FOREIGN KEY(EMP_REF_ID)
+    references EMPLOYEE(Id),
+    FOREIGN KEY(CLIENT_REF_ID)
+    references CLIENT(ID)
+    );
+    
+SELECT * FROM EMPLOYEE;
+SELECT * FROM CLIENT;
+select * from project;
+INSERT INTO PROJECT(ID, EMP_REF_ID, NAME, START_DATE, CLIENT_REF_ID) VALUES
+(1, 1,  'A', '2021-04-21', 3),
+(2, 2, 'B', '2021-03-12', 1),
+(3, 3, 'C', '2021-01-16', 5),
+(4, 3, 'D','2021-04-27',  2),
+(5, 5, 'E', '2021-05-01', 4);
+-- inner join
+-- enlist all the employee id's list along with project allocated to them
+SELECT E.ID, E.F_NAME, E.L_NAME, P.ID, P.NAME FROM EMPLOYEE AS E
+INNER JOIN PROJECT AS P ON E.ID = P.EMP_REF_ID;
+
+-- if we want all columns
+SELECT * FROM EMPLOYEE 
+INNER JOIN PROJECT ON EMPLOYEE.ID = PROJECT.EMP_REF_ID;
+
+-- fetch all employee id's and their contact details who have been working 
+-- jaipur along with the clients names working in hydrabad
+ SELECT E.ID, E.EMAIL_ID, E.PHONE_NO, C.F_NAME, C.L_NAME FROM EMPLOYEE AS E
+ INNER JOIN CLIENT AS C ON E.ID = C.EMP_REF_ID WHERE E.CITY = 'Jaipur' AND
+ C.CITY = 'Hydrabad';
+ -- can we use inner join without using join?
+ --  yes
+ SELECT E.ID, E.EMAIL_ID, E.PHONE_NO, C.F_NAME, C.L_NAME FROM EMPLOYEE AS E, CLIENT AS C 
+ WHERE E.ID = C.EMP_REF_ID AND E.CITY = 'Jaipur' AND
+ C.CITY = 'Hydrabad';
+ 
+-- left join
+-- fect out each project allocated to each employee
+SELECT * FROM EMPLOYEE AS E 
+LEFT JOIN PROJECT AS P ON E.ID = P.EMP_REF_ID;
+
+-- RIGHT JOIN
+-- list out all the project along with the employee's name and their respective
+-- allocated email id
+SELECT P.ID, P.NAME, E.F_NAME, E.L_NAME FROM PROJECT AS P
+RIGHT JOIN EMPLOYEE AS E ON P.EMP_REF_ID = E.ID;
+
+-- CROSS JOIN
+-- list out all the combinations of employee name and projects
+SELECT E.F_NAME, P.NAME FROM EMPLOYEE AS E
+CROSS JOIN PROJECT AS P;
+
+-- USING SET OPERATIONS
+CREATE TABLE DEPARTMANET_1 (
+	EMP_ID INT NOT NULL PRIMARY KEY,
+    NAME VARCHAR(25),
+    ROLE VARCHAR(30)
+);
+CREATE TABLE DEPARTMANET_2 (
+	EMP_ID INT NOT NULL PRIMARY KEY,
+    NAME VARCHAR(25),
+    ROLE VARCHAR(30)
+);
+ALTER TABLE DEPARTMANET_1 RENAME DEPARTMENT_1;
+ALTER TABLE DEPARTMANET_2 RENAME DEPARTMENT_2;
+ INSERT INTO DEPARTMENT_1(EMP_ID, NAME, ROLE) VALUES
+ (1, 'A',  'engineer'),
+ (2, 'B',  'salesman'),
+ (3, 'C',  'manager'),
+ (4, 'D',  'salesman'),
+ (5, 'E',  'engineer');
+ INSERT INTO DEPARTMENT_2(EMP_ID, NAME, ROLE) VALUES
+ (3, 'C',  'manager'),
+ (6, 'F',  'marketing'),
+ (7, 'G',  'salesman');
+ 
+ -- list out all the employess in the comapany
+ SELECT * FROM DEPARTMENT_1
+ UNION
+ SELECT * FROM DEPARTMENT_2;
+ -- list out all the employee in all department who works as salesman
+SELECT * FROM DEPARTMENT_1 WHERE ROLE = 'salesman'
+UNION
+SELECT * FROM DEPARTMENT_2 WHERE ROLE = 'salesman';
+ -- list out all the employee who work in all department
+ SELECT DEPARTMENT_1.* FROM DEPARTMENT_1 
+ INNER JOIN DEPARTMENT_2 USING(EMP_ID);
+ 
+-- list out all the employee working in department_1 but not in department_2
+SELECT DEPARTMENT_1.* FROM DEPARTMENT_1
+LEFT JOIN DEPARTMENT_2 USING(EMP_ID) WHERE DEPARTMENT_2.EMP_ID IS NULL;
+
+-- SUB QUERIES
+--  WHER CLAUSE SAME TABLE
+SELECT * FROM EMPLOYEE WHERE AGE IN(SELECT AGE FROM EMPLOYEE WHERE AGE > 30);
+
+-- whre clause different table
+SELECT * FROM PROJECT;
+-- employee name who is working more than in 1 projects
+SELECT * FROM EMPLOYEE WHERE ID IN (SELECT EMP_REF_ID FROM PROJECT GROUP BY EMP_REF_ID HAVING COUNT(EMP_REF_ID) > 1);
+-- EPLAIN: first we filter emp_ref_id from project based on  count
+-- then with this result we  filter employee 
+
+-- single value subquery
+-- employee details having age > avg(age)
+SELECT * FROM EMPLOYEE WHERE AGE > (SELECT AVG(AGE) FROM EMPLOYEE);
+
+-- from clause - derieved table
+-- select max age person whose first name contains 'a'
+SELECT MAX(AGE) FROM (SELECT * FROM EMPLOYEE WHERE F_NAME  LIKE '%A%') AS TEMP;
+
+-- CREATING CUSTOMIZED VIEW SCHEMA 
+-- IT WON'T CREATE A NEW TABLE
+CREATE VIEW custom_employee AS SELECT F_NAME, L_NAME FROM EMPLOYEE;
+
+-- viewing the view
+SELECT * FROM custom_employee;
+
+-- alter view
+alter VIEW custom_employee AS SELECT F_NAME, L_NAME, ID FROM EMPLOYEE;
